5cf6191c30eee84f0c57adfab3d7335bcd634aad
kozo <kozo.sakuragawa@gmail.com>
2014-11-13 10:03:56 +0900

    first

---
+++ View/Partial/PartialView.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\View\Partial;
+
+//App::uses('View', 'View');
+//App::uses('Folder', 'Utility');
+
+class PartialView extends \Cake\View\View {
+    public $partialCache = 'partial';
+
+    public function partial($name, array $data = array(), array $options = array()) {
+        $file = $plugin = null;
+
+        if (!isset($options['callbacks'])) {
+            $options['callbacks'] = false;
+        }
+
+        if (isset($options['cache'])) {
+            $contents = $this->_elementCache($name, $data, $options);
+            if ($contents !== false) {
+                return $contents;
+            }
+        }
+
+        $file = $this->_getPartialFileName($name);
+        if ($file) {
+            return $this->_renderElement($file, $data, $options);
+        }
+
+        if (empty($options['ignoreMissing'])) {
+            list ($plugin, $name) = pluginSplit($name, true);
+            $name = str_replace('/', DS, $name);
+            $file = $plugin . $this->viewPath . DS . '_' . $name . $this->_ext;
+            throw new \Cake\View\Error\MissingElementException($file);
+        }
+    }
+
+    /**
+     * ファイル名を取得する
+     *
+     * @access private
+     * @author sakuragawa
+     */
+    protected function _getPartialFileName($name) {
+        list($plugin, $name) = $this->pluginSplit($name);
+
+        $paths = $this->_paths($plugin);
+        foreach ($paths as $path) {
+            if (file_exists($path . $this->viewPath . DS . '_' . $name . $this->_ext)) {
+                return $path . $this->viewPath . DS . '_' . $name . $this->_ext;
+            }
+        }
+        return false;
+    }
+}
